42a859bf9a33de695c13d77f7c227c53d6345a12
jnvalino <jnvalino@gmail.com>
2012-11-03 23:10:39 -0400

    Created backend main which continually listens and processes requests from frontend.

---
+++ backend/backend.py
@@ -0,0 +1,51 @@
+import json
+import os
+from refmntr import thesqlpopo
+import time
+import zmq
+
+def LoginUser(request):
+    login_hash = thesqlpopo.AuthenticateUser(request)
+    authenticated = login_hash != 0
+    return {"authenticated":authenticated,"login_hash":login_hash}
+
+def GetProfile(request):
+    return thesqlpopo.RetrievePatientInfo(login_hash,request)
+
+def UpdateProfile(login_hash,request):
+    updated = thesqlpopo.ModifyPatientInfo(login_hash,request) or thesqlpopo.AppendPatientInfo(login_hash.request)
+    return {"updated":updated}
+
+def LogoutUser(login_hash):
+    logged_out = thesqlpopo.LogoutSession(login_hash)
+    return {"logged_out":logged_out}
+    
+# Main
+context = zmq.Context()
+socket = context.socket(zmq.REP)
+socket.bind("tcp://*:1390")
+
+while True:
+    in_stream = socket.recv()
+
+    req = json.loads(in_stream)
+
+    # Debug (remove later)
+    print
+    print json.dumps(msg, indent=4)
+    print
+
+    method = req["method"]
+
+    if method == "login":
+        response = LoginUser(req["request"])
+    elif method == "getprofile":
+        response = GetProfile(req["login_hash"],req["request"])
+    elif method == "updateprofile":
+        response = UpdateProfile(req["login_hash"],req["request"])
+    elif method == "logout":
+        response = LogoutUser(req["login_hash"])
+
+    rep = {"method":method,"response":response}
+    out_stream = json.JSONEncoder().encode(rep)
+    socket.send(out_stream)
