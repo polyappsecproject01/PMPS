2c05e2b4a8bc59ad953081f89b578788e1350c06
Anthony C <acanda01@students.poly.edu>
2012-11-04 16:48:58 -0500

    MySQLdb Initial Connection Code and Related Files

---
+++ backend/dbinfo.py
@@ -0,0 +1,3 @@
+# This file locally stores the database localhost access information with read only local read permissions
+def getdbinfo():
+	return('pmps', 'JzMdfCyWWyzfWz4L', 'pmps') # dbusername, pw, dbname
