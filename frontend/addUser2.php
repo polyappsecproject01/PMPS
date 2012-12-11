d4fb1c5d1e101affb5d67cde327e3fa2fe6378ca
Raybit <raybit.tang@gmail.com>
2012-12-11 15:42:00 -0500

    add create&delete user account functions

---
+++ frontend/addUser2.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Patient Medical Profile System</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<!-- Google Customized Web Fonts !-->
+<link href='https://fonts.googleapis.com/css?family=Raleway:200' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Candal' rel='stylesheet' type='text/css'>
+
+<link rel="stylesheet" href="style.css" />
+</head>
+<body>
+<script type="text/javascript">
+function whenHover(p) {
+	p.src="logo_pmpsc_on.png";
+}
+
+function afterHover(p) {
+	p.src="logo_pmpsc.png";
+}
+</script>
+<div id="container">
+    <header>
+       <img id="logoPic" src="logo_pmpsc.png" onmouseover="whenHover(this)" onmouseout="afterHover(this)"/>
+    </header>
+	<section id="processInfo">
+    <br />
+	<?php
+		  session_start();
+		
+		  if (!empty($_SESSION["access"]))
+		  {
+			  if ($_SESSION["access"] == "admin") {
+					require_once 'validation2.php';
+					$userAccName = cleanData(trim($_POST["userAccountName"]));
+					$userAccPW = cleanData(trim($_POST["userAccountPW"]));
+					$userAccPW2= cleanData(trim($_POST["userAccountPW2"]));
+
+					$pageAccType = cleanData(trim($_POST["userAccountType"]));
+					$userAccType ="";
+
+					// the type name used here is hardcode at html side.
+					if ($pageAccType==='EMTType'){
+						$userAccType = 'readonly';
+					}else if($pageAccType==='DOCType'){
+						 
+						$userAccType = 'readwrite';
+					}
+					
+
+		
+					
+					if ( (!empty($userAccName)) && (!empty($userAccPW)) && (!empty($userAccPW2))  ){
+						if( $userAccPW !=$userAccPW2 ){
+							print (" The two time you enter the password is not consistent.");
+						}else if($userAccType==="admin"){
+							//shoundn't enter into this section
+							print (" The admin role can not be added.");
+								
+						}else if( ($userAccType!="readwrite") && ($userAccType!="readonly")){
+							//shoundn't enter into this section
+							print (" Please enter valid role type.");
+
+						}else{
+							$auth_dataArr = array('username' => $_SESSION["username"], 'ip_address' => $_SERVER['REMOTE_ADDR'], 'login_hash' => $_SESSION["loginHash"]);
+							
+							$requestArr = array('username' => $userAccName, 'accesslevel' => $userAccType, 'password' => $userAccPW);
+							$arr = array('method' => 'addnewuser', 'auth_data' => $auth_dataArr,'request' => $requestArr );
+							$requestJSON = json_encode($arr);
+							$requestJSONEnc = mcrypt_encrypt(MCRYPT_3DES,$ks,$requestJSON,MCRYPT_MODE_ECB);
+							$responseJSONEnc = sendJSONgetJSON($requestJSONEnc);
+							$responseJSON = mcrypt_decrypt(MCRYPT_3DES,$ks,$responseJSONEnc,MCRYPT_MODE_ECB);
+							// Encryption/Decryption algorithm is adding padding to conform to the block-size. Null-characters are needed to be removed from the end by rtrim function
+							$responseArr = json_decode(rtrim($responseJSON, "\0"),true);
+							$method = $responseArr["method"];
+							$authNum = $responseArr["response"]["added"];
+							$timeout = $responseArr["response"]["timeout"];
+						
+							if ($method == "addnewuser")  {
+								if ($authNum === 1) {
+									print ("<i>$userAccName account </i> was successfully added.");
+								} else if ($authNum === 0){
+									if($timeout ===1 ){
+										print("Your session is timeout, you need relogin.");
+									}else{
+										print ("Error: Could not add <i>$userAccName</i> account.");	
+									}
+									
+								}else{
+									print("Error: added num.");
+
+								}
+									
+							} else print("Error: JSON Response");
+
+						}
+
+					} else{
+						print "Please enter valid info in all fields.";
+					} 
+			  }
+			  else {
+				  	if (!empty($_SESSION["errorMsgLogout"]))
+						print '<p style="color:#BE2C07;">'.$_SESSION["errorMsgLogout"].'</p>';
+						
+					 print "{$_SESSION["username"]}: You are not autherized to use this function. Logging of for security purposes.";
+					 session_unset();
+					 session_destroy();
+			  }
+		  }
+		  else print "You need to login first before using the system.";
+		if ($timeout===1)
+				print '<p><a href="index2.php">Return to Login Page</a></p>';
+		else if ($_SESSION["access"] == "readonly")
+			   print '<p><a href="home_emt2.php">Return to Home Page</a></p>';
+		else if ($_SESSION["access"] == "readwrite")
+			   print '<p><a href="home_doctor2.php">Return to Home Page</a></p>';	
+		else if ($_SESSION["access"] == "admin")
+			   print '<p><a href="home_admin2.php">Return to Home Page</a></p>';					 
+	?>
+
+ 
+	</section>
+   	<footer>
+    	<p>Copyright © Team PMPS</p>
+    </footer>
+</div>
+</body>
+</html>
---
+++ frontend/remUser2.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Patient Medical Profile System</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<!-- Google Customized Web Fonts !-->
+<link href='https://fonts.googleapis.com/css?family=Raleway:200' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Candal' rel='stylesheet' type='text/css'>
+
+<link rel="stylesheet" href="style.css" />
+</head>
+<body>
+<script type="text/javascript">
+function whenHover(p) {
+	p.src="logo_pmpsc_on.png";
+}
+
+function afterHover(p) {
+	p.src="logo_pmpsc.png";
+}
+</script>
+<div id="container">
+    <header>
+       <img id="logoPic" src="logo_pmpsc.png" onmouseover="whenHover(this)" onmouseout="afterHover(this)"/>
+    </header>
+	<section id="processInfo">
+    <br />
+	<?php
+		  session_start();
+		
+		  if (!empty($_SESSION["access"]))
+		  {
+			  if ($_SESSION["access"] == "admin") {
+					require_once 'validation2.php';
+					
+					$userAccName = cleanData(trim($_POST["usrAccountName"]));
+					//$userAccType = cleanData(trim($_POST["userAccountType"]));
+					
+					if ( !empty($userAccName) ) {
+						if($_SESSION["username"] === $userAccName){
+							print ("Error: You can not remove the account of yourself from the system.");	
+						}else{
+							$auth_dataArr = array('username' => $_SESSION["username"], 'ip_address' => $_SERVER['REMOTE_ADDR'], 'login_hash' => $_SESSION["loginHash"]);
+							$requestArr = array('username' => $userAccName);
+							$arr = array('method' => 'removeuser', 'auth_data' => $auth_dataArr,'request' => $requestArr );
+						
+							$requestJSON = json_encode($arr);
+							$requestJSONEnc = mcrypt_encrypt(MCRYPT_3DES,$ks,$requestJSON,MCRYPT_MODE_ECB);
+							$responseJSONEnc = sendJSONgetJSON($requestJSONEnc);
+							$responseJSON = mcrypt_decrypt(MCRYPT_3DES,$ks,$responseJSONEnc,MCRYPT_MODE_ECB);
+							// Encryption/Decryption algorithm is adding padding to conform to the block-size. Null-characters are needed to be removed from the end by rtrim function
+							$responseArr = json_decode(rtrim($responseJSON, "\0"),true);
+							$method = $responseArr["method"];
+							$authNum = $responseArr["response"]["deleted"];
+							$timeout = $responseArr["response"]["timeout"];
+
+							if ($method == "removeuser"){
+								if ($authNum === 1) {
+									print ("<i>$userAccName </i> was successfuly removed from the system.");
+								} else if ($authNum === 0){
+									if($timeout===1){
+										print ("Your session is timeout, you need relogin.");
+									}else{
+										print ("Error: Could not remove <i>$userAccName </i> from the system.");	
+									}
+								}
+									
+							} else print("Error: JSON Response");
+
+						}
+						
+						
+					} else print "Please enter valid user name.";
+			  }
+			  else {
+				  	if (!empty($_SESSION["errorMsgLogout"]))
+						print '<p style="color:#BE2C07;">'.$_SESSION["errorMsgLogout"].'</p>';
+						
+					 print "{$_SESSION["username"]}: You are not autherized to use this function. Logging of for security purposes.";
+					 session_unset();
+					 session_destroy();
+			  }
+		  }
+		  else print "You need to login first before using the system.";
+
+		if ($timeout===1)
+				print '<p><a href="index2.php">Return to Login Page</a></p>';
+		else if ($_SESSION["access"] == "readonly")
+			   print '<p><a href="home_emt2.php">Return to Home Page</a></p>';
+		else if ($_SESSION["access"] == "readwrite")
+			   print '<p><a href="home_doctor2.php">Return to Home Page</a></p>';	
+		else if ($_SESSION["access"] == "admin")
+			   print '<p><a href="home_admin2.php">Return to Home Page</a></p>';					 
+	?>
+
+ 
+	</section>
+   	<footer>
+    	<p>Copyright © Team PMPS</p>
+    </footer>
+</div>
+</body>
+</html>
